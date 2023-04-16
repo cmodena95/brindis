bf598665491ce5499f3f4dbc5ff70f0d533a331a
camila modena <itscamilla@hotmail.com>
2023-04-16 14:10:07 -0600

    overlay

--- app/javascript/controllers/slide_controller.js
+++ app/javascript/controllers/slide_controller.js
@@ -9,4 +9,10 @@ export default class extends Controller {
     this.menuTarget.classList.toggle("menu-display")
     this.overlayTarget.classList.toggle("d-none")
   }
+
+  close(event) {
+    this.backgroundTarget.classList.remove("bg-fixed")
+    this.menuTarget.classList.remove("menu-display")
+    event.currentTarget.classList.add("d-none")
+  }
 }
