ae6c04af99d75c1ab26622fd5175a9416779777f
camila modena <itscamilla@hotmail.com>
2023-05-05 11:19:30 +0200

    fix

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -5,20 +5,20 @@ class ContactsController < ApplicationController
     @contact = Contact.new(params[:contact])
     @contact.request = request
     authorize @contact
-    # if @contact.deliver
+    if @contact.deliver
       if params[:source] == "workshop"
         redirect_to workshops_path(Workshop.last), notice: "Mensaje enviado con exito!"
       else
         redirect_to showroom_path, notice: "Mensaje enviado con exito!"
       end
-    # else
-      # if params[:source] == "workshop"
-      #   flash.now[:error] = 'No se pudo enviar el mensaje'
-      #   render "workshops/show", status: :unprocessable_entity
-      # else
-      #   flash.now[:success] = 'No se pudo enviar el mensaje'
-      #   render "pages/showroom", status: :unprocessable_entity
-      # end
+    else
+      if params[:source] == "workshop"
+        flash.now[:error] = 'No se pudo enviar el mensaje'
+        render "workshops/show", status: :unprocessable_entity
+      else
+        flash.now[:success] = 'No se pudo enviar el mensaje'
+        render "pages/showroom", status: :unprocessable_entity
+      end
     end
   end
 
